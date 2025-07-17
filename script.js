c6778a4e5b665ec8f058966890f12d6135f2c17d
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-07-17 12:22:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+let isPlaying = true;
+
+
+video.ontimeupdate = () => {
+  if (!video.duration || isNaN(video.duration)) return;
+  const percentage = ((video.currentTime / video.duration) * 100).toFixed(2) + "%";
+  progressBar.style.width = percentage;
+};
+const handleToggle = ()=>{
+		if(isPlaying){
+	video.pause();
+	isPlaying = false;
+	}
+	else{
+	video.play();
+	isPlaying = true;
+	}
+}
+
+const handleRangeChange = (e)=>{
+  if(e.target.name === "volume")
+  {
+     video.volume = e.target.value;
+  }
+  else
+  {
+     video.playbackRate = e.target.value;
+  }
+}
+toggle.onclick = handleToggle;
+
+ ranges.forEach((range)=>{
+	 range.onclick = (e)=>{
+		 handleRangeChange(e);
+	 }
+ })
+ skipButtons.forEach((skipbtn)=>{
+	 skipbtn.onclick = (e)=>{
+		 video.currentTime += +(e.target.dataset.skip)
+	 }
+ })
+
+
